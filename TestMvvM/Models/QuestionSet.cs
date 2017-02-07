6c026edbedb31cce34202290bd9c6300f6cdf572
John Chabot <chabotja@miamioh.edu>
2017-02-07 18:23:48 -0500

    Add files from desktop

---
+++ TestMvvM/Models/QuestionSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace TestMvvM.Models
+{
+    public class QuestionAnswer
+    {
+        public string Question;
+        public string Answer;
+
+        public QuestionAnswer(string question)
+        {
+            Question = question;
+            Answer = "";
+        }
+    }
+
+    public class QuestionAnswerList
+    {
+        public LinkedList<QuestionAnswer> QuestionSet;
+
+        public QuestionAnswerList()
+        {
+            QuestionSet = new LinkedList<QuestionAnswer>();
+        }
+    }
+}
